0e3291ae3810d6e5d8d8fc96b49db3be5a5774b2
zhangruihua <zrhcode@163.com>
2023-10-31 18:39:27 +0800

    完成png转换为raw

--- jpg2png.js
+++ jpg2png.js
@@ -1,20 +1,40 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const request = require("request");
+const images = require("images")
+const pngTool = require("./png2raw");
+
 
 //随机一张320x240的照片
-const imageUrl = "https://picsum.photos/320/240";
+const imageUrl = "https://picsum.photos/240/240";
+
+function onStart() {
+  console.time("image-time")
+  request.get(imageUrl, { encoding: null }, async (err, data, body) => {
+    if (err) {
+      console.log("err:", err)
+    } else {
+      console.timeEnd("image-time")
+      // 保存到文件
+      // fs.writeFileSync("myImage.jpg", body);
+
+      // sharp(body).toFile('my-image.png')
+      await sharp(body).png().toFile("my-image.png")
+
+      images("my-image.png").save('my-image-mini.png', {
+        quality: 50
+      })
+
+      toRaw('my-image.png')
+    }
+  });
+}
 
 
-request.get(imageUrl, { encoding: null }, (err, data, body) => {
-  if (err) {
-    console.log("err:", err);
-  } else {
-    console.log(body);
-    fs.writeFileSync("test2.jpg", body);
-  }
-});
-// const imageFile = fs.readFileSync(imageUrl);
-// console.log(imageFile);
+function toRaw(buffer) {
+  const byteData = pngTool.pngToraw(buffer)
+  fs.writeFileSync('my-image.raw', byteData)
+}
 
-// sharp(file).toFile("test.png");
+
+onStart()
